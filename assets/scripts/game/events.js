9ba77d897df0fffb8a2b6cae74c030e1f8896b53
ccasner <charlottecasner@gmail.com>
2017-08-31 20:48:22 -0400

    Check for win function simplified
    Still need to turn click handlers back on after reset.

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -6,17 +6,6 @@ let gameBoard = [
   '', '', ''
 ]
 
-// if users selects any of these 8 combinations, they win
-// const gameBoardWins = [
-//   [gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X'],
-//   [gameBoard[3], gameBoard[4], gameBoard[6]],
-//   [gameBoard[6], gameBoard[7], gameBoard[8]],
-//   [gameBoard[0], gameBoard[3], gameBoard[6]],
-//   [gameBoard[1], gameBoard[4], gameBoard[7]],
-//   [gameBoard[2], gameBoard[5], gameBoard[8]],
-//   [gameBoard[0], gameBoard[4], gameBoard[8]],
-//   [gameBoard[2], gameBoard[4], gameBoard[6]]
-// ]
 const winningBoards = [
   [0, 1, 2],
   [3, 4, 5],
@@ -27,17 +16,8 @@ const winningBoards = [
   [0, 4, 8],
   [2, 4, 6]
 ]
-// gameBoard[winningBoards[i][0] = ]
 
 let count = 0
-
-// const winner = function () {
-//   if (count >= 5) {
-//     if (gameBoard[0] === gameBoard[1]) {
-//       console.log(gameBoard[0] + ' WINS!')
-//     }
-//   }
-// }
 
 const userXMove = function () {
   if ($(this).text() === '' && count % 2 === 0) {
@@ -66,97 +46,12 @@ const userOMove = function () {
 }
 
 let winner = ''
-// const checkMatch = function () {
-//   if (gameBoard[0] === 'X' && gameBoard[1] === 'X' && gameBoard[2] === 'X') {
-//     winner = 'X'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[3] === 'X' && gameBoard[4] === 'X' && gameBoard[5] === 'X') {
-//     winner = 'X'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[6] === 'X' && gameBoard[7] === 'X' && gameBoard[8] === 'X') {
-//     winner = 'X'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[0] === 'X' && gameBoard[3] === 'X' && gameBoard[6] === 'X') {
-//     winner = 'X'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[1] === 'X' && gameBoard[4] === 'X' && gameBoard[7] === 'X') {
-//     winner = 'X'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[2] === 'X' && gameBoard[5] === 'X' && gameBoard[8] === 'X') {
-//     winner = 'X'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[0] === 'X' && gameBoard[4] === 'X' && gameBoard[8] === 'X') {
-//     winner = 'X'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[2] === 'X' && gameBoard[4] === 'X' && gameBoard[6] === 'X') {
-//     winner = 'X'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[0] === 'O' && gameBoard[1] === 'O' && gameBoard[2] === 'O') {
-//     winner = 'O'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[3] === 'O' && gameBoard[4] === 'O' && gameBoard[5] === 'O') {
-//     winner = 'O'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[6] === 'O' && gameBoard[7] === 'O' && gameBoard[8] === 'O') {
-//     winner = 'O'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[0] === 'O' && gameBoard[3] === 'O' && gameBoard[6] === 'O') {
-//     winner = 'O'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[1] === 'O' && gameBoard[4] === 'O' && gameBoard[7] === 'O') {
-//     winner = 'O'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[2] === 'O' && gameBoard[5] === 'O' && gameBoard[8] === 'O') {
-//     winner = 'O'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[0] === 'O' && gameBoard[4] === 'O' && gameBoard[8] === 'O') {
-//     winner = 'O'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (gameBoard[2] === 'O' && gameBoard[4] === 'O' && gameBoard[6] === 'O') {
-//     winner = 'O'
-//     $('#message').text(winner + ' Wins!')
-//   } else if (count === 9 && winner === '') {
-//     $('#message').text('DRAW!')
-//   }
-//   if (winner !== '' || count === 9) {
-//     $('.element').off('click')
-//   }
-//   return winner
-// }
 
 const checkForWinner = function () {
   if (count >= 5 && count <= 9) {
     checkWinner()
   }
 }
-const clearBoard = function () {
-  count = 0
-  gameBoard = ['', '', '', '', '', '', '', '', '']
-  winner = ''
-  $('.grid').find('.element').text('')
-}
-
-const newGame = function (event) {
-  event.preventDefault()
-  clearBoard()
-}
-
-// if the values in the indexes of the combos === "XXX" X wins
-
-// let win = ''
-// const findCombo = function () {
-//   let indexValue = 0
-//   if (count >= 5) {
-//     for (let i = 0; i < winningBoards.length; i++) {
-//       for (let j = 0; j < 3; j++) {
-//         indexValue = winningBoards[i][j]
-//         win += gameBoard[indexValue]
-//       }
-//     }
-//   }
-//   return win
-// }
-
 
 const checkMatch = function (a, b, c) {
   if (gameBoard[a] === 'X' && gameBoard[b] === 'X' && gameBoard[c] === 'X') {
@@ -165,10 +60,15 @@ const checkMatch = function (a, b, c) {
   } else if (gameBoard[a] === 'O' && gameBoard[b] === 'O' && gameBoard[c] === 'O') {
     winner = 'O'
     $('#message').text(winner + ' Wins!')
+  } else if (count === 9 && winner === '') {
+    $('#message').text('DRAW!')
   }
-
+  if (winner !== '' || count === 9) {
+    $('.element').off('click')
+  }
   return winner
 }
+
 const checkWinner = function () {
   let a = 0
   let b = 0
@@ -179,6 +79,18 @@ const checkWinner = function () {
     c = winningBoards[i][2]
     checkMatch(a, b, c)
   }
+}
+
+const clearBoard = function () {
+  count = 0
+  gameBoard = ['', '', '', '', '', '', '', '', '']
+  winner = ''
+  $('.grid').find('.element').text('')
+}
+
+const newGame = function (event) {
+  event.preventDefault()
+  clearBoard()
 }
 
 module.exports = {
