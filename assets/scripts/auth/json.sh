5920ea091d85341c41f28b645678b2bf980a1bfd
ccasner <charlottecasner@gmail.com>
2017-09-01 09:27:59 -0400

    Successful curl script for sign-in
    Written in json.sh folder for reference.

--- assets/scripts/auth/json.sh
+++ assets/scripts/auth/json.sh
@@ -1,12 +1,28 @@
-#!/bin/bash
+# #!/bin/bash
+#
+#
+# # EMAIL="user1" PASSWORD="user1" PASSWORD_CONFIRMATION="user1" sh assets/scripts/auth/json.sh
+#
+# # API="${API_ORIGIN:-http://httpbin.org}"
+# # URL_PATH="/post"
+# API="${API_ORIGIN:-http://tic-tac-toe.wdibos.com'}"
+# URL_PATH="/sign-up"
+#
+# curl "${API}${URL_PATH}" \
+#   --include \
+#   --request POST \
+#   --header "Content-Type: application/json" \
+#   --data '{
+#     "credentials": {
+#       "email": "'"${EMAIL}"'",
+#       "password": "'"${PASSWORD}"'",
+#       "password_confirmation": "'"${PASSWORD_CONFIRMATION}"'"
+#     }
+#   }'
 
-
-# EMAIL="user1" PASSWORD="user1" PASSWORD_CONFIRMATION="user1" sh assets/scripts/auth/json.sh
-
-# API="${API_ORIGIN:-http://httpbin.org}"
-# URL_PATH="/post"
-API="${API_ORIGIN:-https://ga-library-api.herokuapp.com}"
-URL_PATH="/sign-up"
+# EMAIL="user1" PASSWORD="user1" sh assets/scripts/auth/json.sh
+API="${API_ORIGIN:-http://tic-tac-toe.wdibos.com}"
+URL_PATH="/sign-in"
 
 curl "${API}${URL_PATH}" \
   --include \
@@ -15,8 +31,7 @@ curl "${API}${URL_PATH}" \
   --data '{
     "credentials": {
       "email": "'"${EMAIL}"'",
-      "password": "'"${PASSWORD}"'",
-      "password_confirmation": "'"${PASSWORD_CONFIRMATION}"'"
+      "password": "'"${PASSWORD}"'"
     }
   }'
 
